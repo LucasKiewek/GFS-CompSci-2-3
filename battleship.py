fb002be72777540b3fabe2e20fc4c72ae7d52d8a
Lucas Kiewek <lucaskiewekcode@gmail.com>
2018-12-21 00:51:57 -0600

    Battleship overlap check and edge check

--- battleship.py
+++ battleship.py
@@ -3,12 +3,35 @@ import os
 
 class Ship:
 
-    # ship class
+    board = []
+    for i in range(10):
+        innerBoard = []
+        for j in range(10):
+            innerBoard.append(" ~ ")
+        board.append(innerBoard)
+
     def __init__(self, row, column, orient, holes):
         self.row = row
         self.column = column
         self.orient = orient
         self.holes = holes
+
+    def place_ship(self, num, r, c, o, phH, phV):
+        if o.lower() == "h":
+            for i in range(num):
+                self.board[r][c + i] = phH
+        else:
+            for i in range(num):
+                self.board[r + i][c] = phV
+
+    def display_setup(self):
+
+        self.place_ship(self.holes, self.row, self.column, self.orient, "===", " = ")
+
+        print("\t 1  2  3  4  5  6  7  8  9  10")
+        for i in range(len(self.board)):
+            tem = "".join(self.board[i])
+            print i+1, "\t", tem
 
 
 
@@ -45,10 +68,10 @@ class Board:
     def display_board(self):
 
         self.place_ship(4, self.carrier.row, self.carrier.column, self.carrier.orient, "[--", "===", "-P]", "[ ]", "|||", "[_]")
-        self.place_ship(3, self.battleship.row, self.battleship.column, self.battleship.orient, "<{]", "={]", "[]>", " ^ ", "|*|", "[=]")
-        self.place_ship(2, self.cruiser.row, self.cruiser.column, self.cruiser.orient, "<=[", "]==", "[]>", " ^ ", "| |", "[+]")
+        self.place_ship(3, self.battleship.row, self.battleship.column, self.battleship.orient, "<{]", "={]", "[]>", "|^|", "|*|", "[=]")
+        self.place_ship(2, self.cruiser.row, self.cruiser.column, self.cruiser.orient, "<=[", "]==", "[]>", "|^|", "| |", "[+]")
         self.place_ship(2, self.submarine.row, self.submarine.column, self.submarine.orient, "(==", "==o", "==)", "( )", "|o|", "( )")
-        self.place_ship(1, self.destroyer.row, self.destroyer.column, self.destroyer.orient, "<={", "={]", "[]>", " ^ ", "|^|", "[=]")
+        self.place_ship(1, self.destroyer.row, self.destroyer.column, self.destroyer.orient, "<={", "={]", "[]>", "(^)", "|^|", "[=]")
 
         print("\t 1  2  3  4  5  6  7  8  9  10")
         for i in range(len(self.board)):
@@ -56,12 +79,36 @@ class Board:
             print i+1, "\t", tem
 
 
-def check(num, holes):
-    while (num < 10 - holes) or (not num.isdigit()):
-        print("Your number is invalid")
+def check(num, holes, rORc):
+    while not num.isdigit():
+        num = raw_input("please enter a number: ")
+    num = int(num)
+    while num > 10 - holes:
+        print "Your", rORc, "number is invalid"
         num = raw_input("please try again: ")
+        while not num.isdigit():
+            num = raw_input("please enter a number: ")
+        num = int(num)
 
-    return int(num)-1
+    return num-1
+
+
+def check_overlap(orientation, row, column, holes):
+    lap = False
+    points = []
+    if orientation == "h":
+        for i in range(holes):
+            points.append([row, column+i])
+    else:
+        for i in range(holes):
+            points.append([row+i, column])
+
+    for i in points:
+        if i in taken:
+            lap = True
+    if lap:
+        print "your ships overlap, try different points."
+    return lap
 
 
 def build_ship(ship, holes):
@@ -73,35 +120,40 @@ def build_ship(ship, holes):
     while valid:
         orientation = raw_input(o_q)
         if orientation.lower() == "h":
-            row = raw_input(r_q)
-            while not row.isdigit():
-                print("Your row number is invalid")
-                row = raw_input("please enter a number 1-10: ")
-            row = int(row)-1
+            lap = True
+            while lap:
+                row = raw_input(r_q)
+                while not row.isdigit():
+                    print("Your row number is invalid")
+                    row = raw_input("please enter a number 1-10: ")
+                row = int(row)-1
 
-            column = raw_input(c_q)
-            column = check(column, holes)
-            os.system("clear")
+                column = raw_input(c_q)
+                column = check(column, holes, "column")
+                lap = check_overlap("h", row, column, holes)
+
             for i in range(holes):
                 taken.append([row, column+i])
             valid = False
 
         elif orientation.lower() == "v":
-            column = raw_input(c_q)
-            while not column.isdigit():
-                print("Your column number is invalid")
-                column = raw_input("please enter a number 1-10: ")
-            column = int(column)-1
+            lap = True
+            while lap:
+                row = raw_input(r_q)
+                row = check(row, holes, "row")
 
-            row = raw_input(r_q)
-            row = check(row, holes)
-            os.system("clear")
+                column = raw_input(c_q)
+                while not column.isdigit():
+                    print("Your column number is invalid")
+                    column = raw_input("please enter a number 1-10: ")
+                column = int(column)-1
+                lap = check_overlap("v", row, column, holes)
+
             for i in range(holes):
                 taken.append([row+i, column])
             valid = False
 
         else:
-            os.system("clear")
             print("please enter a valid orientation")
 
     return Ship(row, column, orientation, holes)
@@ -111,14 +163,36 @@ def main():
     global taken
     taken = []
 
-    carrier = build_ship("carrier", 5)
-    battleship = build_ship("battleship", 4)
-    cruiser = build_ship("cruiser", 3)
-    submarine = build_ship("submarine", 3)
-    destroyer = build_ship("destroyer", 2)
+    os.system("clear")
+    board = []
+    for i in range(10):
+        innerBoard = []
+        for j in range(10):
+            innerBoard.append(" ~ ")
+        board.append(innerBoard)
 
-    board = Board(carrier, battleship, cruiser, submarine, destroyer)
-    board.display_board()
+    print("\t 1  2  3  4  5  6  7  8  9  10")
+    for i in range(len(board)):
+        tem = "".join(board[i])
+        print i+1, "\t", tem
+
+    carrier = build_ship("carrier", 5)
+    os.system("clear")
+    Ship.display_setup(carrier)
+    battleship = build_ship("battleship", 4)
+    os.system("clear")
+    Ship.display_setup(battleship)
+    cruiser = build_ship("cruiser", 3)
+    os.system("clear")
+    Ship.display_setup(cruiser)
+    submarine = build_ship("submarine", 3)
+    os.system("clear")
+    Ship.display_setup(submarine)
+    destroyer = build_ship("destroyer", 2)
+    os.system("clear")
+
+    board1 = Board(carrier, battleship, cruiser, submarine, destroyer)
+    board1.display_board()
 
 
 main()
